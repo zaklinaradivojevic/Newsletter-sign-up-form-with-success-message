7537ed03bdc2a35fc1f2c2c8866cf3ca514789e6
Zaklina Radivojevic <83774205+zaklinaradivojevic@users.noreply.github.com>
2023-11-10 12:29:47 +0100

    Create script.js
    created script.js

---
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const signupForm = document.getElementById("signup");
+  const successMessage = document.getElementById("success");
+
+  
+  signupForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Handle form submission, for example, by sending data to a server.
+
+    // Check if the successMessage element exists before accessing its style property
+    if (successMessage) {
+      successMessage.style.display = "block";
+    }
+
+    // Check if the signupForm element exists before accessing its style property
+    if (signupForm) {
+      signupForm.style.display = "none";
+    }
+  });
+
+  // Define dismissSuccessMessage globally
+  window.dismissSuccessMessage = function() {
+    // Check if the successMessage element exists before accessing its style property
+    if (successMessage) {
+      successMessage.style.display = 'none';
+    }
+
+    // Check if the signin element exists before accessing its style property
+    const signin = document.getElementById('signin');
+    if (signin) {
+      signin.style.display = 'block';
+    }
+  };
+});
